216f3ab7294ae16caec625c9fc5f44ee2a01b3ee
dian candra pribadi <diancandra112@gmail.com>
2019-10-28 11:28:21 +0700

    first commit

---
+++ routes/users.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var router = express.Router();
+const model = require('../models/index')
+
+/* GET users listing. */
+router.get('/', async function (req, res, next) {
+  try {
+    const users = await model.users.findAll({});
+    if (users.length !== 0) {
+      res.json({
+        'status': 'OK',
+        'messages': '',
+        'data': users
+      })
+    } else {
+      res.json({
+        'status': 'ERROR',
+        'messages': 'EMPTY',
+        'data': {}
+      })
+    }
+  } catch (err) {
+    res.json({
+      'status': 'ERROR',
+      'messages': err.messages,
+      'data': {}
+    })
+  }
+});
+
+router.post('/', async function (req, res, next) {
+  try {
+    const {
+      name,
+      email,
+      gender,
+      phoneNumber
+    } = req.body;
+    const users = await model.users.create({
+      name,
+      email,
+      gender,
+      phone_number: phoneNumber
+    });
+  if (users) {
+    res.status(201).json({
+      'status': 'OK',
+      'messages': 'User berhasil ditambahkan',
+      'data': users,
+    })
+  }
+ } catch (err) {
+   res.status(400).json({
+     'status': 'ERROR',
+     'messages': err.message,
+     'data': {},
+   })
+ }
+});
+
+module.exports = router;
